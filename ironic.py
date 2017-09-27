4a08c300928bedb1a510cf619c33f59f1ae840b5
Jack Jiang <jackywathy24@gmail.com>
2017-09-27 21:21:02 +1000

    split project

--- ironic.py
+++ ironic.py
@@ -1,79 +1,9 @@
 from PIL import Image
 import math
-from collections import OrderedDict
-import json
+from JsonReader import JsonMap
 
 defaultc = 10
 
-class JsonMap:
-    def __init__(self, mapname="", background_color="#000000", length=9999, width=9999, theme='overworld', entry=(0,64)):
-        self.dict = OrderedDict()
-        self.create_empty()
-        self.set_map_name(mapname)
-        self.set_background_color(background_color)
-        self.set_width(length)
-        self.set_height(width)
-        self.set_theme(theme)
-
-    def set_width(self, length):
-        self.dict['width'] = length
-
-    def set_height(self, width):
-        self.dict['height'] = width
-
-    def set_theme(self, theme):
-        self.dict['theme'] = theme
-
-
-    def set_background_color(self, str):
-        assert str.startswith("#")
-
-        self.dict['background'] = str
-
-    def set_map_name(self, str):
-        self.dict['MapName'] = str
-
-
-    def add_block(self, block, *args):
-
-        if block in self.dict['blocks'] and args in self.dict['blocks'][block]:
-            print(self.dumps())
-            assert "already added" and False
-
-        self.dict['blocks'].setdefault(block, []).append(args)
-
-    def is_block(self, block, *args):
-        if block not in self.dict['blocks']:
-            return False
-
-        return args in self.dict['blocks'][block]
-
-    def add_entity(self, ent, *args):
-        self.dict['entities'].setdefault(ent, []).append((args))
-
-
-
-
-    def create_empty(self):
-        self.dict['MapName'] = ''
-        self.dict['blocks'] = OrderedDict()
-        self.dict['entities'] = OrderedDict()
-        self.dict['background'] = None
-        self.dict['default_entry'] = [0, 64]
-        self.dict['width'] = None
-        self.dict['height'] = None
-
-
-
-    def sort(self):
-        for item in self.dict['blocks'].values():
-            item.sort()
-
-        for item in self.dict['entities'].values():
-            item.sort()
-
-    def dumps(self):
-        return json.dumps(self.dict, indent=4)
 
 
 from os.path import join
